efd35f188c09d93a0f1572966c551a971fda903d
JoshuaWi <JoshuaWi@discovery.co.za>
2022-12-13 09:43:37 +0200

    Day11b

--- src/days/Day11.kt
+++ src/days/Day11.kt
@@ -5,14 +5,15 @@ fun main() {
 
     fun createMonkeys(inputReader: BufferedReader): List<Monkey> {
         val monkeys = mutableListOf<Monkey>()
-        var currentListOfItems = mutableListOf<Int>()
+        var currentListOfItems = mutableListOf<Long>()
         var testStrings = mutableListOf<String>()
-        lateinit var operation: (Int) -> Int
-        lateinit var test: (Int) -> Int
+        var testDiv = 0L
+        lateinit var operation: (Long) -> Long
+        lateinit var test: (Long) -> Int
         inputReader.forEachLine { line ->
             when {
                 line.trim().isEmpty() -> {
-                    monkeys.add(Monkey(0, currentListOfItems, operation, test))
+                    monkeys.add(Monkey(0L, testDiv, currentListOfItems, operation, test))
                     currentListOfItems = mutableListOf()
                     testStrings = mutableListOf()
                 }
@@ -20,7 +21,7 @@ fun main() {
                     val numsString = line.substringAfter(":").trim()
                     val nums = numsString.split(",")
                     for (num in nums) {
-                        currentListOfItems.add(num.trim().toInt())
+                        currentListOfItems.add(num.trim().toLong())
                     }
                 }
                 line.trim().startsWith("Operation:") -> {
@@ -40,7 +41,7 @@ fun main() {
                             operation = if (v == "old") {
                                 {old -> old + old}
                             } else {
-                                {old -> old + v.toInt()}
+                                {old -> old + v.toLong()}
                             }
                         }
                     }
@@ -53,11 +54,12 @@ fun main() {
                 }
                 line.trim().startsWith("If false:") -> {
                     testStrings.add(line.substringAfter(":").trim())
-                    val div = testStrings[0].split(" ").last().trim().toInt()
+                    val div = testStrings[0].split(" ").last().trim().toLong()
+                    testDiv = div
                     val tMonkeyIndex = testStrings[1].split(" ").last().trim().toInt()
                     val fMonkeyIndex = testStrings[2].split(" ").last().trim().toInt()
                     test = {worry ->
-                        if (worry % div == 0) {
+                        if (worry % div == 0L) {
                             tMonkeyIndex
                         } else {
                             fMonkeyIndex
@@ -67,27 +69,47 @@ fun main() {
                 else -> {}
             }
         }
-        monkeys.add(Monkey(0, currentListOfItems, operation, test))
+        monkeys.add(Monkey(0, testDiv, currentListOfItems, operation, test))
         return monkeys.toList()
     }
 
-    fun part1(inputReader: BufferedReader): Int {
-
-        val monkeys = createMonkeys(inputReader)
-//        println(monkeys)
-
-        for (i in 0..19) {
+    fun playRounds(monkeys: List<Monkey>, rounds: Int, worryLevelFun: (Long) -> Long) {
+        for (i in 0..rounds) {
             for (monkey in monkeys) {
                 for (item in monkey.items) {
                     monkey.inspections++
                     var worryLevel = monkey.operation(item)
-                    worryLevel /= 3
+                    worryLevel = worryLevelFun(worryLevel)
                     val nextMonkey = monkey.test(worryLevel)
                     monkeys[nextMonkey].items.add(worryLevel)
                 }
                 monkey.items.clear()
             }
         }
+    }
+
+    fun part1(inputReader: BufferedReader): Long {
+
+        val monkeys = createMonkeys(inputReader)
+//        println(monkeys)
+
+        playRounds(monkeys, 19) {worryLevel -> worryLevel/3}
+
+//        println()
+//        println(monkeys)
+//        println()
+        val result = monkeys.map { it.inspections }.sorted().takeLast(2).reduce{ left, right -> left * right }
+        println(result)
+        println()
+        return result
+    }
+
+    fun part2(inputReader: BufferedReader): Long {
+
+        val monkeys = createMonkeys(inputReader)
+//        println(monkeys)
+        val modProduct = monkeys.map { it.testDiv }.reduce {left, right -> left*right}
+        playRounds(monkeys, 9999) {worryLevel -> worryLevel%modProduct}
 
 //        println(monkeys)
         val result = monkeys.map { it.inspections }.sorted().takeLast(2).reduce{ left, right -> left * right }
@@ -95,20 +117,17 @@ fun main() {
         return result
     }
 
-    fun part2(inputReader: BufferedReader): Int {
-
-        val result = 0
-        println(result)
-        return result
-    }
-
     val testInputReader = readBufferedInput("tests/Day%02d".format(day))
-    check(part1(testInputReader) == 10605)
-//    check(part2(testInputReader) == 0)
+//    check(part1(testInputReader) == 10605L)
+        check(part2(testInputReader) == 2713310158L)
 
     val input = readBufferedInput("input/Day%02d".format(day))
-    println(part1(input))
-//    println(part2(input))
+//    println(part1(input))
+    println(part2(input))
 }
 
-data class Monkey(var inspections: Int, val items: MutableList<Int>, val operation: (Int) -> Int, val test: (Int) -> Int)
+data class Monkey(var inspections: Long,
+                  val testDiv: Long,
+                  val items: MutableList<Long>,
+                  val operation: (Long) -> Long,
+                  val test: (Long) -> Int)
